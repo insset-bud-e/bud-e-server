57cf46643dcc8aa1777c1b6b91babb9e7278b514
Bradawk <niepceron.brad@gmail.com>
2018-06-12 09:59:45 +0200

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify
-from config import SECRET
 from flask_pymongo import PyMongo
 
 server = Flask(__name__)
 
-server.secret_key = SECRET
+server.secret_key = "7bce579c0af059582f18bd5158f22ef78dae5055"
 server.config['MONGO_DBNAME'] = 'bude'
 server.config['MONGO_URI'] = 'mongodb://localhost:27017/bude'
 
